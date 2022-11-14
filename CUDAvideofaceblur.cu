5f35cbd8a9e17d1208fc94f194cf8bec26b2f958
mgomezu <49196702+mgomezu@users.noreply.github.com>
2022-11-14 08:33:36 -0500

    Update CUDAvideofaceblur.cu

--- CUDAvideofaceblur.cu
+++ CUDAvideofaceblur.cu
@@ -21,8 +21,6 @@ using namespace cv;
 __global__ void add(int *a, int *b, int *c) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
     c[index] = a[index] + b[index];
-    cout << c << endl;
-    cout << "Test 2" << endl;
     __syncthreads();
 }
 
